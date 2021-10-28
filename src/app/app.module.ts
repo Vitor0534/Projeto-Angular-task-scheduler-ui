096ca6e9e0ff1539fef0507affc3942e5fac43da
Vitor <vitor.silva@zgsolucoes.com.br>
2021-10-28 09:34:41 -0300

    1#: MSG: Criação inicial do projeto com as dependências do Angular --> Material e ngx-mask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// todos os modulos que serão utilizados devem ser importados aqui
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
